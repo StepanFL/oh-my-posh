ae06b8cd1badd66c5bc9f2926c1d657e8e0c85d4
Stepan <99119737+StepanFL@users.noreply.github.com>
2025-02-09 17:05:47 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,5 @@
 $OutputEncoding = [System.Console]::OutputEncoding = [System.Console]::InputEncoding = [System.Text.Encoding]::UTF8
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\amro.omp.json" | Invoke-Expression
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\1_3.omp.json" | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
