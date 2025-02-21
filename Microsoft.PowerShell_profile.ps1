bee977b0da773b6e28ff3ff2ba72ffab5ffb2780
Stepan <99119737+StepanFL@users.noreply.github.com>
2025-02-21 21:14:43 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
+# setting from PowerShell
 $OutputEncoding = [System.Console]::OutputEncoding = [System.Console]::InputEncoding = [System.Text.Encoding]::UTF8 #use from Uncoding normal Latin simbol
 
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\simple_gwo.omp.json" | Invoke-Expression
