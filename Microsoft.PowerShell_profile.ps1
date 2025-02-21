9e139c571b198ad69c60e5592db55d435603881c
Stepan <99119737+StepanFL@users.noreply.github.com>
2025-02-21 20:47:02 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
-$OutputEncoding = [System.Console]::OutputEncoding = [System.Console]::InputEncoding = [System.Text.Encoding]::UTF8
+$OutputEncoding = [System.Console]::OutputEncoding = [System.Console]::InputEncoding = [System.Text.Encoding]::UTF8 #use from Uncoding normal Latin simbol
 
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\1_3.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\simple_gwo.omp.json" | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
